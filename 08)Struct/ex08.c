27158ff0c4a9d234eb314a2afd1da4f1b84e59af
LFMP <luizfellipe42@hotmail.com>
2017-11-20 23:27:08 -0200

    mod lista8- ex08*

--- 08)Struct/ex08.c
+++ 08)Struct/ex08.c
@@ -3,10 +3,38 @@
 struct dados{
 	char nome[20];
 	int qtdf;
-	char nomefilme[20];
+};
+struct lfilmes{
+	char nomefilme
 	int ano;
 	float duracao;
-};
+}
+
+void criafilmes(int qtdf, int pos){
+	struct lfilmes filmes[pos][qtdf];
+	for (int a = 0; a < qtdf; ++a){
+		printf("Filmes: ");
+		printf("Filme %d: ", a+1);
+		scanf(" %[^\n]s", filmes[pos][a].nomefilme);
+		printf("Ano: ");
+		scanf(" %d", &filmes[pos][a].ano);
+		printf("Duracao: ");
+		scanf(" %f", &filmes[pos][a].duracao);
+	}
+	char direto[20] = "ze ninguem";
+	while(direto != ''){
+		scanf("%[^\n]s", direto);
+		for (int j = 0; j < 5; ++j){
+			if (direto == dir[j].nome){
+				for (int d = 0; d < dir[j].qtdf; ++d){
+					printf("%s\n", filmes[j][d].nomefilme);
+					printf("%d\n", filmes[j][d].ano);
+					printf("%f\n", filmes[j][d].duracao);
+				}
+			}
+		}
+	}
+}
 
 int main(){
 	int n=0, men = 1;
@@ -17,24 +45,6 @@ int main(){
 		scanf(" %[^\n]s",dir[i].nome);
 		printf("Quantidade de filmes: ");
 		scanf("%d", &dir[i].qtdf);
-		printf("Filmes: ");
-		struct dados filmes[dir[i].qtdf]
-		for (int a = 0; a < dir[i].qtdf; ++a){
-			printf("Filme %d: ", a+1);
-			scanf(" %[^\n]s", filmes[a].nome);
-			printf("Ano: ");
-			scanf(" %d", &filmes[a].ano);
-			printf("Duracao: ");
-			scanf(" %f", &filmes[a].duracao);
-		}
-	}
-	char direto[20] = "ze ninguem";
-	while(direto != ''){
-		scanf("%[^\n]s", direto);
-		for (int j = 0; j < 5; ++j){
-			if (direto == dir[j].nome){
-				printf("%s\n", filmes[1].nome);
-			}
-		}
+		criafilmes(dir[i].qtdf, i);
 	}
 }
