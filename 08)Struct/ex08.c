a8480d6c2b496c38f1d40f8e353ee59febd6bdd0
LFMP <luizfellipe42@hotmail.com>
2017-11-24 21:08:47 -0200

    modif lista8 - ex08

--- 08)Struct/ex08.c
+++ 08)Struct/ex08.c
@@ -1,50 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
+struct lfilmes{
+	char nomefilme[50];
+	int ano;
+	float duracao;
+};
 struct dados{
 	char nome[20];
 	int qtdf;
+	struct lfilmes diretor[5];
 };
-struct lfilmes{
-	char nomefilme
-	int ano;
-	float duracao;
-}
-
-void criafilmes(int qtdf, int pos){
-	struct lfilmes filmes[pos][qtdf];
-	for (int a = 0; a < qtdf; ++a){
-		printf("Filmes: ");
-		printf("Filme %d: ", a+1);
-		scanf(" %[^\n]s", filmes[pos][a].nomefilme);
-		printf("Ano: ");
-		scanf(" %d", &filmes[pos][a].ano);
-		printf("Duracao: ");
-		scanf(" %f", &filmes[pos][a].duracao);
-	}
-	char direto[20] = "ze ninguem";
-	while(direto != ''){
-		scanf("%[^\n]s", direto);
-		for (int j = 0; j < 5; ++j){
-			if (direto == dir[j].nome){
-				for (int d = 0; d < dir[j].qtdf; ++d){
-					printf("%s\n", filmes[j][d].nomefilme);
-					printf("%d\n", filmes[j][d].ano);
-					printf("%f\n", filmes[j][d].duracao);
-				}
-			}
-		}
-	}
-}
 
 int main(){
 	int n=0, men = 1;
 	struct dados dir[5];
-	for (int i = 0; i < 5; i++){
+	for (int i = 0; i < 2; i++){
 		printf("Diretor %d\n", i+1);
 		printf("Nome: ");
-		scanf(" %[^\n]s",dir[i].nome);
+		scanf(" %[^\n]s", dir[i].nome);
 		printf("Quantidade de filmes: ");
 		scanf("%d", &dir[i].qtdf);
-		criafilmes(dir[i].qtdf, i);
+		for (int a = 0; a < dir[i].qtdf; ++a){
+			printf("Filme %d: ", a+1);
+			scanf(" %[^\n]s", dir[a].diretor[a].nomefilme);
+			printf("Ano: ");
+			scanf(" %d", &dir[a].diretor[a].ano);
+			printf("Duracao: ");
+			scanf(" %f", &dir[a].diretor[a].duracao);
+		}
 	}
+	
+	char direto[20];
+	do{
+		printf("Digite nome do diretor pra buscar ou 0 para sair: ");
+		scanf("%s", direto);
+		if (strlen(direto) == 0 || strcmp(direto, "") == 0){
+			break;
+		}
+		for (int j = 0; j < 2; ++j){
+			if (strcmp(direto, dir[j].nome) == 0){
+				for (int c = 0; c < dir[j].qtdf; ++c){
+					printf("%s\n", dir[c].diretor[c].nomefilme);
+				}
+			}
+		}
+	}while(direto != NULL);
 }
