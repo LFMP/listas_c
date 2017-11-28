c4d563d7512a4f2c88035372ae411ea056363de3
LFMP <luizfellipe42@hotmail.com>
2017-11-28 16:02:19 -0200

    modif lista8 - ex08

--- 08)Struct/ex08.c
+++ 08)Struct/ex08.c
@@ -9,12 +9,12 @@ struct lfilmes{
 struct dados{
 	char nome[20];
 	int qtdf;
-	struct lfilmes diretor[5];
+	struct lfilmes diretor[2];
 };
 
 int main(){
-	struct dados dir[5];
-	for (int i = 0; i < 5; i++){
+	struct dados dir[2];
+	for (int i = 0; i < 2; i++){
 		printf("Diretor %d\n", i+1);
 		printf("Nome: ");
 		scanf(" %[^\n]s", dir[i].nome);
@@ -32,14 +32,16 @@ int main(){
 	
 	char direto[20];
 	do{
+		direto[0] = '\0';
 		printf("Digite nome do diretor pra buscar: ");
-		scanf(" %[^\n]s", direto);
-		for (int j = 0; j < 5; ++j){
+		getchar();
+		scanf("%[^\n]s", direto);
+		for (int j = 0; j < 2; ++j){
 			if (strcmp(direto, dir[j].nome) == 0){
 				for (int c = 0; c < dir[j].qtdf; ++c){
 					printf("%s\n", dir[c].diretor[c].nomefilme);
 				}
 			}
 		}
-	}while(strlen(direto) > 1);
+	}while(strlen(direto) > 0);
 }
